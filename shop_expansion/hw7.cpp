72efcdf3e4f3907c3eb7e669f2b68cf326dde18a
Lee Guo <yoshi21420@gmail.com>
2019-12-14 22:05:00 +0800

    change tabs to 4 spaces

--- shop_expansion/hw7.cpp
+++ shop_expansion/hw7.cpp
@@ -18,7 +18,7 @@ int ReadInputFile(int costArrPtr[MAX_TASK_NUM][MAX_EMPLOYEE_NUM])
     {
         for (int i=0; i<taskCnt; ++i)
             for (int j=0; j<taskCnt; ++j)
-				fscanf(f, "%d", &costArrPtr[i][j]);
+                fscanf(f, "%d", &costArrPtr[i][j]);
     }
 
     fclose(f);
@@ -35,7 +35,7 @@ bool AlreadyUsed(int solution[MAX_EMPLOYEE_NUM], int c)
 
 double TotalCost(int costArr[MAX_TASK_NUM][MAX_EMPLOYEE_NUM],
                  int solution[MAX_EMPLOYEE_NUM],
-				 int size)
+                 int size)
 {
     double totalCost = 0.0;
     for (int i=0; i<size; ++i)
@@ -50,16 +50,16 @@ int main()
 {
     int costArr[MAX_TASK_NUM][MAX_EMPLOYEE_NUM];
     int totalEmployee = 0;
-	
-	// read file
-	totalEmployee = ReadInputFile(costArr);
-	if (totalEmployee <= 0)
-	{
-		// no employee, return directly
-		return 0;
-	}
     
-	// start reading assignments from user-input
+    // read file
+    totalEmployee = ReadInputFile(costArr);
+    if (totalEmployee <= 0)
+    {
+        // no employee, return directly
+        return 0;
+    }
+    
+    // start reading assignments from user-input
     int solution[MAX_EMPLOYEE_NUM] = {0};
     int nowEmployee = 0;
     while (true)
@@ -69,49 +69,49 @@ int main()
         
         // Error-proof Logic: out of range
         if (solution[nowEmployee] < 0 || solution[nowEmployee] >= totalEmployee)
-		{
-			cout << "Out of range, invalid" << endl;
+        {
+            cout << "Out of range, invalid" << endl;
 
-			// continue: ignore the remaining logic of current loop and continue straightly to "next loop"
-			continue;
-		}
-		
-		// Error-proof Logic: duplicated assignment
-		if (AlreadyUsed(solution, nowEmployee))
-		{
-			cout << solution[nowEmployee] << " duplicate, invalid" << endl;
+            // continue: ignore the remaining logic of current loop and continue straightly to "next loop"
+            continue;
+        }
+        
+        // Error-proof Logic: duplicated assignment
+        if (AlreadyUsed(solution, nowEmployee))
+        {
+            cout << solution[nowEmployee] << " duplicate, invalid" << endl;
 
-			// continue: ignore the remaining logic of current loop and continue straightly to "next loop"
-			continue;
-		}
-		
-		// successful assignment, next one!
-		++nowEmployee;
-		
-		// assignments done?
-		if (nowEmployee == totalEmployee)
-		{
-			// break: ignore the remaining logic of current loop and "leave the loops"
-			break;
-		}
+            // continue: ignore the remaining logic of current loop and continue straightly to "next loop"
+            continue;
+        }
+        
+        // successful assignment, next one!
+        ++nowEmployee;
+        
+        // assignments done?
+        if (nowEmployee == totalEmployee)
+        {
+            // break: ignore the remaining logic of current loop and "leave the loops"
+            break;
+        }
     }
-	// ps. "continue" and "break" used above work for both "while-loop" and "for-loop"
-	
-	cout << "Total cost = " << TotalCost(costArr, solution, totalEmployee) << endl;
-	cout << "Your assignment" << endl;
-	for (int i=0; i<totalEmployee; ++i)
-	{
-		/*
-		 * semantic:
-		 *    value = (evaluation_logic) ? A : B;
-		 *
-		 * meaning:
-		 *    if the 'evaluation_logic' is true, then the value is assigned as A;
-		 *    if the 'evaluation_logic' is false, then the value is assigned as B;
-		 */
-		char separator = (i == totalEmployee - 1) ? '\n' : ',';
-		cout << solution[i] << separator;
-	}
+    // ps. "continue" and "break" used above work for both "while-loop" and "for-loop"
+    
+    cout << "Total cost = " << TotalCost(costArr, solution, totalEmployee) << endl;
+    cout << "Your assignment" << endl;
+    for (int i=0; i<totalEmployee; ++i)
+    {
+        /*
+         * semantic:
+         *    value = (evaluation_logic) ? A : B;
+         *
+         * meaning:
+         *    if the 'evaluation_logic' is true, then the value is assigned as A;
+         *    if the 'evaluation_logic' is false, then the value is assigned as B;
+         */
+        char separator = (i == totalEmployee - 1) ? '\n' : ',';
+        cout << solution[i] << separator;
+    }
 
     return 0;
 }
